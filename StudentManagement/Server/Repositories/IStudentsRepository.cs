9e34ffa0eb31ebbc70d60d72bdeac983dd32dc95
Kronos_Codeur <piraliarris@gmail.com>
2024-01-04 13:22:40 +0000

    students repo

--- StudentManagement/Server/Repositories/IStudentsRepository.cs
+++ StudentManagement/Server/Repositories/IStudentsRepository.cs
@@ -2,11 +2,11 @@ using StudentManagement.Shared.models;
 
 namespace StudentManagement.Server.Repositories;
 
-public interface IStudentRepositories
+public interface IStudentsRepository
 {
     Task<List<Student>> GetAllStudents();
     Task<Student> SaveStudent(Student student);
     Task<Student?> GetStudentDetails(int id);
     Task<Student?> UpdateStudentInfo(int id, Student student);
-    void DeleteStudent(int id);
+    Task<bool> DeleteStudent(int id);
 }
