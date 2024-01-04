0198af7381606a9360d8e42a2c2b1a5545db7d2f
Kronos_Codeur <piraliarris@gmail.com>
2024-01-04 14:00:54 +0000

    Student Controller done

--- StudentManagement/Server/Controllers/StudentsController.cs
+++ StudentManagement/Server/Controllers/StudentsController.cs
@@ -21,4 +21,40 @@ public class StudentsController:ControllerBase
         var students = await _studentsService.GetAllStudents();
         return Ok(students);
     }
+    [HttpGet("GetStudentDetails/{id}")]
+    public async Task<IActionResult> GetStudentDetails(int id)
+    {
+        var student = await _studentsService.GetStudentDetails(id);
+        if (student is null)
+        {
+            return NotFound("Student not found");
+        }
+        return Ok(student);
+    }
+    [HttpPost("SaveStudent")]
+    public async Task<ActionResult<Student>> SaveStudent(Student student)
+    {
+        var newStudent  = await _studentsService.SaveStudent(student);
+        return CreatedAtAction(nameof(GetStudentDetails),newStudent,newStudent.Id);
+    }
+    [HttpPut("UpdateStudentInfo/{id}")]
+    public async Task<IActionResult> UpdateStudentInfo(int id,Student student)
+    {
+        var result  = await _studentsService.UpdateStudentInfo(id,student);
+        if (result is null)
+        {
+            return NotFound("Student not found");
+        }
+        return Ok(result);
+    }
+    [HttpPut("DeleteStudent/{id}")]
+    public async Task<IActionResult> DeleteStudent(int id)
+    {
+        var result  = await _studentsService.DeleteStudent(id);
+        if (!result)
+        {
+            return NotFound("Student not found");
+        }
+        return NoContent();
+    }
 }
