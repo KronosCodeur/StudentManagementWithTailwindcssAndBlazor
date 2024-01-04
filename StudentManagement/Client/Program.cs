3d7ea46c261e1ae1c9815f82fba35fda10dd2c83
Kronos_Codeur <piraliarris@gmail.com>
2024-01-04 11:25:29 +0000

    tailwind config

--- StudentManagement/Client/Program.cs
+++ StudentManagement/Client/Program.cs
@@ -6,9 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("StudentManagement.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddHttpClient("StudentManagement.ServerAPI",
+    client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("StudentManagement.ServerAPI"));
+builder.Services.AddScoped(
+    sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("StudentManagement.ServerAPI"));
 
 await builder.Build().RunAsync();
